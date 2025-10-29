a1188c46f9d72e7c7345c101054b04548035ca14
Michael O'Hanrahan <104068149+michaelohanrahan@users.noreply.github.com>
2025-10-29 09:40:05 +0100

    Update __init__.py

--- hydromt_wflow/__init__.py
+++ hydromt_wflow/__init__.py
@@ -1,16 +1,8 @@
 """HydroMT plugin for wflow models."""
 
-<<<<<<< HEAD
-from .version import __version__
-from .wflow import WflowModel
-from .wflow_sediment import WflowSedimentModel
-
-__all__ = ["WflowModel", "WflowSedimentModel"]
-=======
 from hydromt_wflow.version import __version__
 from hydromt_wflow.wflow_base import WflowBaseModel
 from hydromt_wflow.wflow_sbm import WflowSbmModel
 from hydromt_wflow.wflow_sediment import WflowSedimentModel
 
 __all__ = ["WflowBaseModel", "WflowSbmModel", "WflowSedimentModel"]
->>>>>>> v1.0.0rc2
